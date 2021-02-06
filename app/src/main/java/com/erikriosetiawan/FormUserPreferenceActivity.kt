726e43a176e734c99c4ed08ab597b78a6e86887c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-06 13:53:16 +0700

    Create FormUserPreferenceActivity.kt

---
+++ app/src/main/java/com/erikriosetiawan/FormUserPreferenceActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.mysharedpreferences.R
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}

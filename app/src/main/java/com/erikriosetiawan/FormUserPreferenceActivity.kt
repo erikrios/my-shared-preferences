e78e2c564d9fc6eb5cc156718366f58d7417d868
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-06 13:54:05 +0700

    Integrated the view binding

--- app/src/main/java/com/erikriosetiawan/FormUserPreferenceActivity.kt
+++ app/src/main/java/com/erikriosetiawan/FormUserPreferenceActivity.kt
@@ -1,12 +1,16 @@
 package com.erikriosetiawan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.erikriosetiawan.mysharedpreferences.R
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.mysharedpreferences.databinding.ActivityFormUserPreferenceBinding
 
 class FormUserPreferenceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormUserPreferenceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_user_preference)
+        binding = ActivityFormUserPreferenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
